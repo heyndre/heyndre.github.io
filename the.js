58334315a99eea648d84ab7b9ea5f7ef89b34b26
enddre <pandegaf@gmail.com>
2018-12-07 08:01:31 +0700

    js alert

--- the.js
+++ the.js
@@ -7,3 +7,21 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+function alertInput() {
+    var a = document.getElementById("namadepan").value;
+    var b = document.getElementById("email").value;
+    var c = document.getElementById("notelp").value;
+    var e = document.getElementById("kategori").value;
+        var h = document.getElementById("judull").value;
+    var i = document.getElementById("rincian").value;
+
+    if (a != "" && b != "" && c != "" && d != "" && e != "" && f != "" &&
+        h != "" && i != "" && j != "" && k != "" && l != "" && m != "") {
+        alert("Terima kasih, aspirasi anda telah disimpan!")
+    }
+    else {
+    alert("Maaf Semua Data tidak boleh kosong !");
+    }
+    }
